3ed2a5a69ff86eac5d77f0ee35db71662ef2126c
Wrong <ilak71363@gmail.com>
2022-02-07 13:18:00 +0300

    Описание интерфейсов

--- Wcf_Chat/IServiceChat.cs
+++ Wcf_Chat/IServiceChat.cs
@@ -8,11 +8,23 @@ using System.Text;
 namespace Wcf_Chat
 {
     // ПРИМЕЧАНИЕ. Можно использовать команду "Переименовать" в меню "Рефакторинг", чтобы изменить имя интерфейса "IServiceChat" в коде и файле конфигурации.
-    [ServiceContract]
+    [ServiceContract(CallbackContract = typeof(IServiceChatCallBack))]
     public interface IServiceChat
     {
         [OperationContract]
-        void DoWork();
+        int Connect();
 
+        [OperationContract]
+        void Disconnect(int id);
+
+        [OperationContract(IsOneWay = true)]
+        void SendMessage(string msg);
+
+    }
+
+    public interface IServiceChatCallBack
+    {
+        [OperationContract]
+        void MsgCallback(string msg);
     }
 }
