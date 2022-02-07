409d0515fddfdbda4af3a399f3fb11e6bb5e24ca
Wrong <ilak71363@gmail.com>
2022-02-07 17:48:53 +0600

    Хост

---
+++ ChatHost/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace ChatHost
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var host = new ServiceHost(typeof(Wcf_Chat.ServiceChat)))
+            {
+                Console.WriteLine("Хост запущен");
+                Console.ReadKey();
+            }
+        }
+    }
+}
