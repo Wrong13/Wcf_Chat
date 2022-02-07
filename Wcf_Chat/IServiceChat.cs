87c4d0a1c535dcd37d64e632b12a6728db71e485
Wrong <ilak71363@gmail.com>
2022-02-07 13:11:58 +0300

    Добавьте файлы проекта.

---
+++ Wcf_Chat/IServiceChat.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Wcf_Chat
+{
+    // ПРИМЕЧАНИЕ. Можно использовать команду "Переименовать" в меню "Рефакторинг", чтобы изменить имя интерфейса "IServiceChat" в коде и файле конфигурации.
+    [ServiceContract]
+    public interface IServiceChat
+    {
+        [OperationContract]
+        void DoWork();
+
+    }
+}
